438dc0ddb8e9b34196b1bb31a005cf4c09804337
Leocaldio Carlos <leocaldiocarloss@gmail.com>
2025-03-25 22:22:50 +0100

    Feat: Add Alter function

--- LivrariaControleEmprestimo.WEB/Controllers/ClienteController.cs
+++ LivrariaControleEmprestimo.WEB/Controllers/ClienteController.cs
@@ -6,7 +6,10 @@ namespace LivrariaControleEmprestimo.WEB.Controllers
 {
     public class ClienteController : Controller
     {
+
         private ClienteServices oClienteRepository = new ClienteServices();
+
+
         public IActionResult Index()
         {
             List<Cliente> oListCLiente = oClienteRepository.orepositoryCliente.SelecionarTodos();
@@ -16,7 +19,44 @@ namespace LivrariaControleEmprestimo.WEB.Controllers
        
         public IActionResult Create()
         {
+           
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Cliente model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            oClienteRepository.orepositoryCliente.Incluir(model);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Details(int id)
+        {
+           Cliente cliente= oClienteRepository.orepositoryCliente.SelecionarPK(id);
+            return View(cliente);
+        }
+
+        public IActionResult Edit(int id) 
+        {
+            Cliente cliente = oClienteRepository.orepositoryCliente.SelecionarPK(id);
+            return View(cliente);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Cliente model) 
+        {
+            Cliente ocliente = oClienteRepository.orepositoryCliente.Alterar(model);
+            
+            int id = ocliente.Id;
+
+            return RedirectToAction("Details", new { id });
+        }
+
     }
 }
